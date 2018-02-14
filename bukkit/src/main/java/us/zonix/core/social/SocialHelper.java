61578df4b2db3dabcc1ad2a5bf5013153ff1cc7d
Emilio <devemilio@hotmail.com>
2018-02-14 08:27:17 -0600

    removed messages request

--- bukkit/src/main/java/us/zonix/core/social/SocialHelper.java
+++ bukkit/src/main/java/us/zonix/core/social/SocialHelper.java
@@ -4,7 +4,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import us.zonix.core.CorePlugin;
-import us.zonix.core.api.request.MessageRequest;
 import us.zonix.core.profile.Profile;
 import us.zonix.core.rank.Rank;
 
@@ -12,18 +11,7 @@ public class SocialHelper {
 
     private static CorePlugin main = CorePlugin.getInstance();
 
-    private String[] messages = new String[] {
-            "We are currently having a 25% Sale on store.keapvp.com",
-            "If you Butterfly click you will recieve a punishment you use DCPrevent",
-            "Check out our Twitter for updates and giveaways! twitter.com/KeaPvP",
-            "Staff applications are currrenty being accepted come into ts.keapvp.com ",
-            "We have a TeamSpeak server! ts.keapvp.com",
-            "See a hacker/cheater? Report them using /report",
-            "Leaderboards will be up very soon!",
-            "Have any problems come into ts.keapvp.com"};
-
     public void sendMessage(Player from, Profile fromProfile, Player to, Profile toProfile, String message) {
-        //main.getRequestProcessor().sendRequestAsync(new MessageRequest.InsertRequest(from.getUniqueId(), "(To " + to.getName() + ") -> " + message));
 
         toProfile.setLastMessaged(from.getUniqueId());
         fromProfile.setLastMessaged(to.getUniqueId());
