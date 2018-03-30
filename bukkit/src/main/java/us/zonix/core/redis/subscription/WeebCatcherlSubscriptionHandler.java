15002dd4a6eb26b519fd61cc29efa00155d2ef61
Emilio <devemilio@hotmail.com>
2018-03-30 02:33:14 -0600

    added online check

--- bukkit/src/main/java/us/zonix/core/redis/subscription/WeebCatcherlSubscriptionHandler.java
+++ bukkit/src/main/java/us/zonix/core/redis/subscription/WeebCatcherlSubscriptionHandler.java
@@ -2,6 +2,7 @@ package us.zonix.core.redis.subscription;
 
 import com.google.gson.JsonObject;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import us.zonix.core.CorePlugin;
 import us.zonix.core.punishment.PunishmentType;
@@ -19,11 +20,15 @@ public class WeebCatcherlSubscriptionHandler implements JedisSubscriptionHandler
             case "ban":
                 String name = object.get("name").getAsString();
                 String check = object.get("check").getAsString();
+                Player target = Bukkit.getPlayer(name);
 
-                new BukkitRunnable() {
-                public void run() {
-                    new PunishmentHelper(Bukkit.getConsoleSender(), name, PunishmentType.BAN, "Client: " + check, (long) -1, true, false);
-                }}.runTaskAsynchronously(CorePlugin.getInstance());
+                if(target != null) {
+                    new BukkitRunnable() {
+                        public void run() {
+                            new PunishmentHelper(Bukkit.getConsoleSender(), name, PunishmentType.BAN, "Client: " + check, (long) -1, true, false);
+                        }
+                    }.runTaskAsynchronously(CorePlugin.getInstance());
+                }
                 break;
         }
     }
